e7fe73434668a3ef14e85f23877cda144c635fd1
Davoidsj <alokpathik56@gmail.com>
2023-09-02 17:30:31 +0600

    die

---
+++ die.php
@@ -0,0 +1,15 @@
+<?php 
+
+echo "1.Send Messages"."<br/>";
+echo "2.Send Messages"."<br/>";
+echo "3.Send Messages"."<br/>";
+exit('This is error message it does not work.');
+
+echo "4.Send Messages";
+echo "5.Send Messages";
+echo "6.Send Messages";
+
+
+
+
+?>
